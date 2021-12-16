52062dde35ee9e233b8a2118d7dbcecdbb82761f
Lucas <29230467+Somfic@users.noreply.github.com>
2021-12-16 18:14:24 +0100

    Added reactive background color based on user profile

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,7 @@ import { goto } from '$app/navigation';
 
 	import Button from '$lib/button.svelte';
 	import { onMount } from 'svelte';
+	import {extract} from "$lib/colors";
 
 	import {spotify, logout} from '../lib/spotify';
 
@@ -14,12 +15,15 @@ import { goto } from '$app/navigation';
 
 	onMount(async () => {
 		user = (await spotify.getMe()).body;
+		var colors = await extract(user?.images[0].url);
+		backgroundColor = colors.DarkMuted.hex;
 	});
 
 	var user: SpotifyApi.CurrentUsersProfileResponse = undefined;
+	var backgroundColor = "#000000";
 </script>
 
-<main>
+<main style={"background-color: " + backgroundColor + ";"}>
 	{#if user == undefined}
 		<h1>One sec</h1>
 	{:else}
@@ -35,5 +39,13 @@ import { goto } from '$app/navigation';
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
+
+		transition: background-color 1s ease;
+	}
+
+	img {
+		width: 400px;
+		height: 400px;
+		border-radius: 5px;
 	}
 </style>
