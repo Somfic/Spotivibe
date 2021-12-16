abb78ded83950c9e1c28b6769e7a5f7f8bfba42a
Lucas <29230467+Somfic@users.noreply.github.com>
2021-12-16 18:55:18 +0100

    Lazy image loading

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,33 +3,48 @@
 </script>
 
 <script lang="ts">
-import { goto } from '$app/navigation';
+	import { goto } from '$app/navigation';
 
 	import Button from '$lib/button.svelte';
 	import { onMount } from 'svelte';
-	import {extract} from "$lib/colors";
+	import { extract } from '$lib/colors';
 
-	import {spotify, logout} from '../lib/spotify';
+	import { spotify, logout } from '../lib/spotify';
 
-	import SpotifyWebApi from "spotify-web-api-node";
+	import SpotifyWebApi from 'spotify-web-api-node';
+	import Image from '$lib/image.svelte';
 
 	onMount(async () => {
 		user = (await spotify.getMe()).body;
 		var colors = await extract(user?.images[0].url);
 		backgroundColor = colors.DarkMuted.hex;
+
+		var playback = (await spotify.getMyCurrentPlaybackState()).body;
+
+		if(playback?.is_playing) {
+			var track = (await spotify.getTrack(playback.item.id)).body;
+			console.log(playback.progress_ms / track.duration_ms * 100 + "%")
+		}
 	});
 
 	var user: SpotifyApi.CurrentUsersProfileResponse = undefined;
-	var backgroundColor = "#000000";
+	var backgroundColor = '#000000';
 </script>
 
-<main style={"background-color: " + backgroundColor + ";"}>
+<main style={'background-color: ' + backgroundColor + ';'}>
 	{#if user == undefined}
 		<h1>One sec</h1>
 	{:else}
 		<h1>Hi {user?.display_name}</h1>
-		<img src={user?.images[0].url} alt={user?.display_name} />
-		<Button type="cancel" text="Logout" action={() => {logout(); goto("/login")}} />
+		<Image source={user?.images[0].url} width={400} height={400} />
+		<Button
+			type="cancel"
+			text="Logout"
+			action={() => {
+				logout();
+				goto('/login');
+			}}
+		/>
 	{/if}
 </main>
 
