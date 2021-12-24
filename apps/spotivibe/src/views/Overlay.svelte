85c6f93b5a43b14e523dfa978e60be84ade3a242
Lucas <29230467+Somfic@users.noreply.github.com>
2021-12-24 03:11:48 +0100

    Added progressbar chapters

--- apps/spotivibe/src/views/Overlay.svelte
+++ apps/spotivibe/src/views/Overlay.svelte
@@ -22,29 +22,64 @@
         <div class="song">
           <h1 class="fat">{$current.song?.name}</h1>
           <h2>{$current.song?.artists[0].name}</h2>
-          <div class="progress-bar" style="background-color: {$current.colors?.Muted.hex};">
-            <div class="progress" style="background-color: {$current.colors?.LightVibrant.hex}; width: {$current.elapsed / $current?.song?.duration_ms * 100000}%;" />
+          <div class="progress-bar">
+            {#each Array($current?.analysis?.sections?.length) as _, i}
+              <div
+                class="progress-chapter"
+                style="
+                height: {-1/$current.analysis?.sections[i].loudness * 20}px;
+                background-color: {$current.colors?.DarkMuted.hex}; 
+                width: {($current?.analysis?.sections[i].duration /
+                  $current?.song?.duration_ms) *
+                  100000}%;"
+              >
+                <div
+                  class="progress"
+                  style="
+                  background-color: {$current.colors?.LightVibrant.hex}; 
+                  width: {$current.elapsed > $current.analysis?.sections[i].start ? (($current.elapsed - $current.analysis?.sections[i].start) / $current.analysis?.sections[i].duration * 100) : 0}%;"
+                />
+              </div>
+            {/each}
           </div>
         </div>
       </div>
       <div class="colors">
-        <div class="color" style="background-color: {$current.colors?.LightMuted.hex};">
+        <div
+          class="color"
+          style="background-color: {$current.colors?.LightMuted.hex};"
+        >
           <h1>Light Muted</h1>
         </div>
-        <div class="color" style="background-color: {$current.colors?.Muted.hex};">
+        <div
+          class="color"
+          style="background-color: {$current.colors?.Muted.hex};"
+        >
           <h1>Muted</h1>
         </div>
-        <div class="color" style="background-color: {$current.colors?.DarkMuted.hex};">
+        <div
+          class="color"
+          style="background-color: {$current.colors?.DarkMuted.hex};"
+        >
           <h1>Dark Muted</h1>
         </div>
 
-        <div class="color" style="background-color: {$current.colors?.LightVibrant.hex};">
+        <div
+          class="color"
+          style="background-color: {$current.colors?.LightVibrant.hex};"
+        >
           <h1>Light Vibrant</h1>
         </div>
-        <div class="color" style="background-color: {$current.colors?.Vibrant.hex};">
+        <div
+          class="color"
+          style="background-color: {$current.colors?.Vibrant.hex};"
+        >
           <h1>Vibrant</h1>
         </div>
-        <div class="color" style="background-color: {$current.colors?.DarkVibrant.hex};">
+        <div
+          class="color"
+          style="background-color: {$current.colors?.DarkVibrant.hex};"
+        >
           <h1>Dark Vibrant</h1>
         </div>
       </div>
@@ -100,7 +135,7 @@
           border-radius: 10px;
           width: 90px;
           height: 90px;
-          margin: .5rem;
+          margin: 0.5rem;
           transition: background-color 1s ease;
           box-shadow: 0 0 100px 10px rgba(0, 0, 0, 0.25);
 
@@ -140,15 +175,27 @@
             width: 100%;
             border-radius: 100px;
             margin-top: 1rem;
-            box-shadow: 0 0 100px 10px rgba(0, 0, 0, 0.25);
             margin-bottom: -1rem;
             opacity: 0.7;
             transition: background-color 2s ease;
+            display: flex;
+            align-items: center;
 
-            .progress {
-              height: 2px;
+            .progress-chapter {
+              margin: 0px 1.5px;
               border-radius: 100px;
-              transition: background-color 2s ease;
+              min-height: 2.5px;
+              max-height: 10px;
+              transition: all 200ms ease;
+              box-shadow: 0 0 100px 10px rgba(0, 0, 0, 0.25);
+
+              .progress {
+                width: 0%;
+                height: 100%;
+                border-radius: 100px;
+                transition: background-color 200ms ease;
+                max-width: 100%;
+              }
             }
           }
         }
